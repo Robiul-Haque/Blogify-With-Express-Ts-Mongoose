5206651435ab13796aff30addff2a53351299c50
Robiul-Haque <robiulcoc420@gmail.com>
2025-01-25 21:56:25 +0600

    change otp code expire time 5 min

--- src/utils/generateOtp.ts
+++ src/utils/generateOtp.ts
@@ -1,10 +1,10 @@
 import crypto from 'crypto';
 const generateOtp = () => {
-    // return Math.floor(100000 + Math.random() * 900000);
     // Generate a 6-digit OTP.
     const otp = crypto.randomInt(100000, 999999).toString();
+
     // Set OTP expiry time (10 minutes from now).
-    const otpExpiry = new Date(Date.now() + 10 * 60 * 1000);
+    const otpExpiry = new Date(Date.now() + 5 * 60 * 1000);
     return { otp, otpExpiry };
 }
 
