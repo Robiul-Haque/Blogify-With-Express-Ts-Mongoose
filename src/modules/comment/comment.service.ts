fdc8949d8ba14478ad4a8f73551aade4cc3bf2e7
Robiul-Haque <robiulcoc420@gmail.com>
2025-01-31 01:29:55 +0600

    add blog model for update comment in array with blog id

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,8 +1,10 @@
+import { Blog } from "../blog/blog.model";
 import { TComment } from "./comment.interface";
 import { Comment } from "./comment.model";
 
 const createCommentIntoDB = async (payload: TComment) => {
     const res = await Comment.create(payload);
+    await Blog.findByIdAndUpdate(payload.blog, { $push: { comments: res._id } });
     return res;
 }
 
