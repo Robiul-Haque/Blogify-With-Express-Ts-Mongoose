aa3d3004083018f115060ca89e0654b07c5432cc
Robiul-Haque <robiulcoc420@gmail.com>
2025-02-04 13:34:13 +0600

    add verifyToken middleware in admin route

--- src/modules/admin/admin.route.ts
+++ src/modules/admin/admin.route.ts
@@ -9,18 +9,18 @@ import verifyToken from "../../middleware/verifyToken";
 const router = Router();
 
 // Endpoint to get admin from the database.
-router.get("/get-admin", adminController.getAdmin);
+router.get("/get-admin", verifyToken("admin"), adminController.getAdmin);
 
 // Endpoint to update admin from the database.
-router.patch("/update-admin", upload.single("image"), bodyParser, validateRequest(adminValidation.updateAdminSchema), adminController.updateAdmin);
+router.patch("/update-admin", verifyToken("admin"), upload.single("image"), bodyParser, validateRequest(adminValidation.updateAdminSchema), adminController.updateAdmin);
 
 // Endpoint to get all users from the database.
-router.get("/get-all-user", adminController.getAllUser);
+router.get("/get-all-user", verifyToken("admin"), adminController.getAllUser);
 
 // Endpoint to update user blocked status in the database.
-router.patch("/update-user-blocked", validateRequest(adminValidation.updateUserBlockedSchema), adminController.updateUserBlocked);
+router.patch("/update-user-blocked", verifyToken("admin"), validateRequest(adminValidation.updateUserBlockedSchema), adminController.updateUserBlocked);
 
 // Endpoint to delete a user from the database.
-router.delete("/delete-user/:id", adminController.deleteUser);
+router.delete("/delete-user/:id", verifyToken("admin"), adminController.deleteUser);
 
 export const adminRoutes = router;
