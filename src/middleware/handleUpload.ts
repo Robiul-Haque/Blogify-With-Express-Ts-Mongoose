2ea691d0bc48b096b8a4ef6ecaaf59c213637715
Robiul-Haque <robiulcoc420@gmail.com>
2025-02-14 00:53:03 +0600

    change cloudinary img format type

--- src/middleware/handleUpload.ts
+++ src/middleware/handleUpload.ts
@@ -15,7 +15,7 @@ const storage = new CloudinaryStorage({
     cloudinary,
     params: {
         folder: "Blogify",
-        allowed_formats: ["jpg", "png"],
+        allowed_formats: ["JPG", "jpg", "png"],
     } as any,
 });
 
