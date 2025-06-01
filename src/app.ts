d52a077429c5dafccb685de1f0dfc7577805a38a
Robiul-Haque <robiulcoc420@gmail.com>
2025-06-01 21:07:44 +0600

    change cors function & add new domain to complete first stage development

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,19 @@ import notFound from './middleware/notFound';
 import globalErrorHandler from './middleware/globalErrorHandler';
 import router from './router';
 const app: Application = express();
+const allowedOrigins = ["https://blogify0.netlify.app/", "http://localhost:5173", "http://localhost:5173"];
 
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+}));
 app.use(cookieParser());
 app.use('/api/v1', router);
 
