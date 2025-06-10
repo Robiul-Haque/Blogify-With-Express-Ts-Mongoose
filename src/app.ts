6f88bc9dac0bae69e26b21ac032e51ed732cc5d5
Robiul Haque <robiulcoc420@gmail.com>
2025-06-10 21:26:02 +0600

    change cors origin url

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,10 @@ import router from './router';
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors({ origin: ["https://blogify0.netlify.app/", "http://localhost:5173", "http://localhost:5174"], credentials: true }));
+app.use(cors({ origin: ["https://blogify0.netlify.app", "http://localhost:5173", "http://localhost:5174"], credentials: true }));
 app.use(cookieParser());
 app.use('/api/v1', router);
+app.options('*', cors());
 
 // Default route to confirm whether the server is running or not
 app.get('/', (req: Request, res: Response) => {
