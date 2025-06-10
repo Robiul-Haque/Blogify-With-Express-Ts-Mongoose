44b79e1a0b4c96ad79e2ac4633606a8a7a19fdc8
Robiul Haque <robiulcoc420@gmail.com>
2025-06-10 20:33:44 +0600

    add new origin for cors error issue

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,9 @@ import notFound from './middleware/notFound';
 import globalErrorHandler from './middleware/globalErrorHandler';
 import router from './router';
 const app: Application = express();
-const allowedOrigins = ["https://blogify0.netlify.app/", "http://localhost:5173", "http://localhost:5173"];
 
 app.use(express.json());
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
-}));
+app.use(cors({ origin: ["https://blogify0.netlify.app/", "http://localhost:5173", "http://localhost:5173"], credentials: true }));
 app.use(cookieParser());
 app.use('/api/v1', router);
 
